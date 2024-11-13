86615ce7b0db9ca8f7baf4ada3cfb2b590a4730c
Prem Choudhary <pk5480524@gmail.com>
2024-11-13 14:51:50 +0530

    String methods

---
+++ StringMethods/index.js
@@ -0,0 +1,42 @@
+// charAt()
+let str = "Hello";
+console.log(str.charAt(0)); // H
+
+// concat(): Combines two or more strings.
+let str1 = "Hello";
+let str2 = "World";
+console.log(str1.concat(" ", str2)); // Hello World
+
+
+// includes(): Checks if a string contains a specified substring.
+let Hello = "Hello World";
+console.log(Hello.includes("World")); // true
+
+
+// indexOf(): Returns the index of the first occurrence of a specified value.
+let str3 = "Hello World";
+console.log(str3.indexOf("o")); // 4
+
+
+// slice(): Extracts a part of a string and returns a new string.
+let strr = "Hello World";
+console.log(strr.slice(0, 5)); // Hello
+
+// split(): Splits a string into an array of substrings.
+let sttr = "Hello World";
+console.log(sttr.split(" ")); // ["Hello", "World"]
+
+
+// toLowerCase(): Converts a string to lowercase.
+let strcase = "Hello World";
+console.log(strcase.toLowerCase()); // hello world
+
+// toUpperCase(): Converts a string to uppercase.
+let hello = "Hello World";
+console.log(hello.toUpperCase()); // HELLO WORLD
+
+//
+
+
+
+
