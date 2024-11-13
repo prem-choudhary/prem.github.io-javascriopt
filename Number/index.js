501d6c1b977a9aa5f3001ba8fa0ae81f5b741cf6
Prem Choudhary <pk5480524@gmail.com>
2024-11-13 15:21:45 +0530

    Number methods

---
+++ Number/index.js
@@ -0,0 +1,11 @@
+
+// toFixed(): Formats a number using fixed-point notation.
+let num = 5.56789;
+console.log(num.toFixed(2)); // 5.57
+
+
+//  toString(): Returns a string representing the specified number.
+let num1 = 123;
+console.log(num1.toString()); // "123"
+
+// 
