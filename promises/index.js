05c7068ac92748ac86ea881f4d9f42b37d581974
Prem Choudhary <pk5480524@gmail.com>
2024-11-14 16:35:54 +0530

    promises methods

--- promises/index.js
+++ promises/index.js
@@ -10,3 +10,13 @@ const myPromise = new Promise((resolve, reject) => {
     }
   });
   
+
+//   You handle the fulfillment or rejection of a promise using the .then() and .catch() methods.
+
+myPromise
+  .then((message) => {
+    console.log(message); // This will run if the promise is resolved
+  })
+  .catch((error) => {
+    console.log(error); // This will run if the promise is rejected
+  });
