2bb82e96fef08dc9d39545071d9f59fcbf5e9df6
Prem Choudhary <pk5480524@gmail.com>
2024-12-08 17:03:05 +0530

    String Method

---
+++ String/index.js
@@ -0,0 +1,10 @@
+let name = "Alice";
+let greeting = `Hello, ${name}! How are you?`;
+console.log(greeting); // Output: Hello, Alice! How are you?
+
+let multiline = `This is a
+multiline string.`;
+console.log(multiline);
+// Output:
+// This is a
+// multiline string.
