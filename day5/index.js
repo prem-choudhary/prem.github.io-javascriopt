a57bffd6ca1eab921c58eebb7a395f0b1d3cc2ae
Prem Choudhary <pk5480524@gmail.com>
2024-12-12 12:03:57 +0530

    Heart- shape

--- day5/index.js
+++ day5/index.js
@@ -5,7 +5,7 @@ for (let i = n / 2; i < n; i += 2) {
   for (let j = 1; j < n - i; j += 2) {
     str += " ";
   }
-  for (let j = 1; j < i + 1; j++) {
+  for (let j = 1; j < i + 1; j++) { 
     str += "*";
   }
   for (let j = 1; j < n - i + 1; j++) {
