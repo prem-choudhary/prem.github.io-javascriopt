a3581c7cff2993f5ffb88def4d2ebcb2beb7a594
Prem Choudhary <pk5480524@gmail.com>
2024-12-17 13:05:49 +0530

    map methods

---
+++ Array/map.js
@@ -0,0 +1,9 @@
+let users = [
+  { name: "Alice", age: 25 },
+  { name: "Bob", age: 30 },
+  { name: "Charlie", age: 35 },
+];
+
+let names = users.map((user) => user.name);
+
+console.log(names); // ['Alice', 'Bob', 'Charlie']
