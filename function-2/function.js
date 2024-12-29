a3d300e6eb60f8054a2d673b365afaa199c7e56b
Prem Choudhary <pk5480524@gmail.com>
2024-12-29 11:30:11 +0530

    function-javascript

---
+++ function-2/function.js
@@ -0,0 +1,14 @@
+// Function declaration
+// function greet(name) {
+//   return "Hello " + name;
+// }
+
+// Function expression
+const greet = function (name) {
+  return "Hello " + name;
+};
+
+// Arrow function
+const greets = (name) => "Hello " + name;
+
+console.log(greets("Ishita")); // Output: Hello Alice
