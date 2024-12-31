8628ec9bc3f8eb34d733d2bc6ac8ace08f6e367d
Prem Choudhary <pk5480524@gmail.com>
2024-12-31 10:59:47 +0530

    object

---
+++ mockinterview/index.js
@@ -0,0 +1,22 @@
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log(`${this.name} makes a noise.`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name) {
+    super(name);
+  }
+
+  speak() {
+    console.log(`${this.name} Prem.`);
+  }
+}
+
+const dog = new Dog("Choudhary");
+dog.speak(); // Output: Rex barks.
