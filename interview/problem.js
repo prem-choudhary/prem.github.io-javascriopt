ada49508bc4d704ee407eaca489c2fc4e6acea75
Prem-choudhary <pk5480524@gmail.com>
2026-03-24 16:15:33 +0530

    SubString

---
+++ interview/problem.js
@@ -0,0 +1,25 @@
+let str = "masai";
+
+for (let i = 0; i < str.length; i++) {
+  let sub = "";
+
+  for (let j = i; j < str.length; j++) {
+    sub += str[j];
+    console.log(sub);
+  }
+}
+/* 
+m
+ma
+mas
+masa
+masai
+a
+as
+asa
+s
+sa
+a
+ai
+i
+*/
